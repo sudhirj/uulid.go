6f3912d0b8232921221a646d8f816323d65e5c9c
Sudhir Jonathan <sudhir.j@gmail.com>
2019-12-16 13:05:28 +0530

    Added time only ULIDs for querying

--- uulid.go
+++ uulid.go
@@ -3,6 +3,7 @@ package uulid
 import (
 	"github.com/google/uuid"
 	"github.com/oklog/ulid/v2"
+	"time"
 )
 
 type UULID [16]byte
@@ -13,4 +14,15 @@ func (uulid UULID) asUUID() uuid.UUID {
 
 func (uulid UULID) asULID() ulid.ULID {
 	return ulid.ULID(uulid)
+}
+
+func TimeAsULID(t time.Time) ulid.ULID {
+	return ulid.MustNew(ulid.Timestamp(t), ZeroReader{})
+}
+
+type ZeroReader struct{}
+
+func (zR ZeroReader) Read(b []byte) (int, error) {
+	b[0] = 0
+	return 1, nil
 }
--- uulid_test.go
+++ uulid_test.go
@@ -1,1 +1,33 @@
 package uulid
+
+import (
+	"bytes"
+	"github.com/oklog/ulid/v2"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestTimeAsULID(t *testing.T) {
+	type args struct {
+		t time.Time
+	}
+	tests := []struct {
+		name string
+		args args
+		want ulid.ULID
+	}{
+		{
+			"t1",
+			args{t: time.Unix(1576481036, 999999999)},
+			ulid.MustNew(ulid.Timestamp(time.Unix(1576481036, 999999999)), bytes.NewReader([]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TimeAsULID(tt.args.t); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TimeAsULID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
